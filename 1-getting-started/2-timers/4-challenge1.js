d5939c1a9631b4af7ee658c139b1284b4c4b7e10
angelo acosta <angelocastellanos99@gmail.com>
2019-08-31 12:40:50 -0500

    Solved challenge timer - challenge 01

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,10 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
+const theOneFunc = palabra => {
+    console.log('Hello after ' + palabra + ' seconds');
+};
 
 // Hello after 4 seconds
+setTimeout(theOneFunc, 4 * 1000, 4);
 
 // Hello after 8 seconds
-
+setTimeout(theOneFunc, 8 * 1000, 8);
 // You can define only ONE function
