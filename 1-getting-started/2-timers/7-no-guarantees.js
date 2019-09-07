eacd6deb02533b73b4db3c6a828a279292017a18
angelo acosta <angelocastellanos99@gmail.com>
2019-09-07 07:18:35 -0500

    Solved challenge timeer 2 - challenge 01

--- 1-getting-started/2-timers/7-no-guarantees.js
+++ 1-getting-started/2-timers/7-no-guarantees.js
@@ -6,3 +6,11 @@ setTimeout(
 for (let i = 0; i < 1e10; i++) {
   // Block Node Synchronously
 }
+
+// var count = 0;
+// const test1 = () => console.log('Hello after 0.5 seconds. MAYBE!');
+// const test = setTimeout(test1, 500);
+
+// for (let i = 0; i < 1e10; i++) {
+//   console.log(test1);
+// }
--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,26 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+// const n = setTimeout(
+//     () => console.log('Hello after 0.5 seconds. MAYBE!'),
+//     500,
+// );
+
+// for (let i = 0; i < 1e10; i++) {
+//     // Block Node Synchronously
+// }
+
+let counter = 0;
+
+const myF = () =>{
+    console.log("Hello World");
+    counter += 1;
+
+    if (counter == 5){
+        console.log("Done!");
+        // clearInterval(timerId);
+    }
+}
+
+const timerId = setInterval(myF,1000);
